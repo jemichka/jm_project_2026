4cd9c5a6e96d7b1b99f6269f46bae026af567d3b
jemichka <jemik73@mail.ru>
2026-05-16 03:18:42 +0300

    jm_7

--- src/views/Ads/NewAdView.vue
+++ src/views/Ads/NewAdView.vue
@@ -1,15 +1,110 @@
 <template>
   <v-container>
-    <v-row justify="center">
-      <h1>New Ad View</h1>
+    <v-row>
+      <v-col cols="8" offset="2">
+        <h1 class="text--secondary mb-3 mt-3">Create Ad</h1>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="8" offset="2">
+        <v-form v-model="valid" ref="form" validation>
+          <v-text-field
+            name="title"
+            label="Ad Title"
+            type="text"
+            v-model="title"
+            :rules="[(v) => !!v || 'Title is required']"
+          >
+          </v-text-field>
+          
+          <v-textarea
+            name="description"
+            label="Ad Description"
+            type="text"
+            v-model="description"
+            :rules="[(v) => !!v || 'Description is required']"
+            class="mb-3"
+          ></v-textarea>
+        </v-form>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="8" offset="2">
+        <v-btn class="mt-3" color="warning">
+          Upload
+          <v-icon right dark>mdi-cloud-upload</v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="8" offset="2">
+        <img
+          src="https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg"
+          height="150"
+          class="mt-3"
+        />
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="8" offset="2">
+        <v-switch v-model="promo" label="Add to Promo?"></v-switch>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="8" offset="2">
+        <v-spacer></v-spacer>
+        <v-btn
+          color="success"
+          @click="createAd"
+          :loading="loading"
+          :disabled="!valid || loading"
+        >Create Ad</v-btn>
+      </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
 export default {
-  data () {
+  data () { 
     return {
+      valid: false,
+      title: "",
+      description: "",
+      promo: true,
+      loading: false
+    } 	
+  },
+  methods: {
+    createAd() {
+      if (this.$refs.form.validate()) {
+        this.loading = true;
+        
+        const ad = {
+          title: this.title,
+          desc: this.description,
+          promo: this.promo,
+          src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+          id: Date.now().toString()
+        }
+        
+        console.log(ad);
+        
+        // Имитация загрузки
+        setTimeout(() => {
+          this.loading = false;
+          // Очистка формы после создания
+          this.title = "";
+          this.description = "";
+          this.promo = true;
+          this.$refs.form.resetValidation();
+        }, 1000);
+      }
     }
   }
 }
